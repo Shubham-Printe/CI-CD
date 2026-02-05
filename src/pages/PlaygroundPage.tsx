888422bec01db7320f0a595609c22d2734e3119e
Shubham-Printe <shubhamprinte99@gmail.com>
2026-02-05 13:09:47 +0530

    initial commit

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,10 @@
-import Onboarding from "../components/Onboarding";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  
+  const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias architecto quos magnam accusamus distinctio quidem in, odit quis! Laborum adipisci beatae a totam, error similique repellendus distinctio facere, recusandae tempore ducimus maxime fugit, culpa aliquam minima odio voluptatem nostrum saepe. Dolorem dignissimos officiis ipsam rerum? Esse ipsa tempora nesciunt aliquid consequuntur iste omnis ex nulla exercitationem! Culpa veniam dolore temporibus perferendis corrupti hic exercitationem fugit dicta consequuntur voluptatibus. Quo molestiae ipsam quaerat adipisci cupiditate unde quasi expedita quod maiores ab necessitatibus ratione facere voluptatum totam consectetur id ullam nobis, inventore error modi, recusandae non iste fugit molestias. Eligendi, quia repellat."
+
+  return <ExpandableText text={text} />;
 };
 
 export default PlaygroundPage;
